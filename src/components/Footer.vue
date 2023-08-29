d94509ca243693ab3089ea73b44d4f1f7da0603c
wezzganteng <fawwazalfaroz@gmail.com>
2023-08-29 16:37:38 +0700

    ecommerce

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="relative mt-20 bg-gray-900 px-4 pt-20">
-  <div class="absolute -top-10 left-1/2 h-16 w-16 -translate-x-1/2 rounded-xl border-4 border-sky-500 bg-white p-2"><img class="h-full object-contain" src="/images/logo-circle.png" alt="" /></div>
+  <div class="absolute -top-10 left-1/2 h-16 w-16 -translate-x-1/2 rounded-xl border-4 border-sky-500 bg-white p-2"><img class="h-full object-contain" src="" alt="" /></div>
   <nav aria-label="Footer Navigation" class="mx-auto mb-10 flex max-w-lg flex-col gap-10 text-center sm:flex-row sm:text-left">
     <a href="#" class="font-medium text-white">Demo</a>
     <a href="#" class="font-medium text-white">Support</a>
